598e0629635e52a444825111066bc35e18a7d550
Marco <marco.helmich@live.com>
2016-10-19 20:41:18 -0700

    - stubbing out internal node groups - converting all users to lists and a more OO programming model

--- dist-bc-service/src/test/java/org/distbc/data/structures/SibPlusTree/InternalNodeGroupTest.java
+++ dist-bc-service/src/test/java/org/distbc/data/structures/SibPlusTree/InternalNodeGroupTest.java
@@ -1,7 +1,13 @@
 package org.distbc.data.structures.SibPlusTree;
 
+import org.junit.Test;
+
 /**
  * Created by mhelmich on 10/19/16.
  */
 public class InternalNodeGroupTest {
+    @Test
+    public void testBasic() throws Exception {
+        InternalNodeGroup ing = new InternalNodeGroup(1, 3, 2);
+    }
 }
