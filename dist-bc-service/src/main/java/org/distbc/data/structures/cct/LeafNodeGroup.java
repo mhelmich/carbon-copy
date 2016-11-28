9236b3e4c4d377797a8272383a6138de784ec9d7
Marco <marco.helmich@live.com>
2016-11-27 20:12:39 -0800

    - second cleaner implementation -- let's see how that goes

---
+++ dist-bc-service/src/main/java/org/distbc/data/structures/cct/LeafNodeGroup.java
@@ -0,0 +1,135 @@
+package org.distbc.data.structures.cct;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+class LeafNodeGroup<K extends Comparable<K>, V extends Comparable<V>> extends NodeGroup<K> {
+
+    private final ArrayList<ArrayList<K>> keys;
+    private final ArrayList<ArrayList<V>> values;
+
+    @VisibleForTesting
+    LeafNodeGroup next;
+    @VisibleForTesting
+    LeafNodeGroup previous;
+
+    LeafNodeGroup(int nodeSize, int numNodes) {
+        super(nodeSize, numNodes);
+        this.keys = initKeyLists();
+
+        Vector<ArrayList<V>> vvs = new Vector<>(numNodes);
+        vvs.setSize(numNodes);
+        this.values = new ArrayList<>(vvs);
+        for (int i = 0; i < numNodes; i++) {
+            this.values.set(i, newValueList());
+        }
+    }
+
+    private ArrayList<V> newValueList() {
+        Vector<V> vv = new Vector<>(nodeSize);
+        vv.setSize(nodeSize);
+        return new ArrayList<>(vv);
+    }
+
+    @VisibleForTesting
+    LeafNodeGroup<K, V> split() {
+        LeafNodeGroup<K, V> newLng = new LeafNodeGroup<>(this.nodeSize, this.numNodes);
+        List<ArrayList<K>> subListOldLngKeys = this.keys.subList(this.numNodes / 2, this.numNodes);
+        List<ArrayList<V>> subListOldLngValues = this.values.subList(this.numNodes / 2, this.numNodes);
+
+        for (int i = 0; i < subListOldLngKeys.size(); i++) {
+            for (int j = 0; j <subListOldLngKeys.get(i).size(); j++) {
+                newLng.put((i * nodeSize) + j, subListOldLngKeys.get(i).get(j), subListOldLngValues.get(i).get(j));
+            }
+        }
+
+        newLng.previous = this;
+        newLng.next = this.next;
+        if (this.next != null) {
+            this.next.previous = newLng;
+        }
+        this.next = newLng;
+
+        for (int i = (this.numNodes / 2) * this.nodeSize; i < this.numNodes * this.nodeSize; i++) {
+            delete(i);
+        }
+
+        return newLng;
+    }
+
+    @VisibleForTesting
+    void shiftOneRight(int from, int to) {
+        for (int i = to; i > from; i--) {
+            K key = getKey(i - 1);
+            V value = getValue(i - 1);
+            put(i, key, value);
+        }
+
+        delete(from);
+    }
+
+    private void delete(int idx) {
+        // when passing in nulls
+        // put takes care of the bit set
+        // and the number of empty slots
+        put(idx, null, null);
+    }
+
+    @Override
+    K getKey(int index, int offset) {
+        return this.keys.get(index).get(offset);
+    }
+
+    private V getValue(int index, int offset) {
+        return this.values.get(index).get(offset);
+    }
+
+    @VisibleForTesting
+    K getKey(int idx) {
+        Pair<Integer, Integer> p = relativeAddress(idx);
+        return getKey(p.getLeft(), p.getRight());
+    }
+
+    @VisibleForTesting
+    V getValue(int idx) {
+        Pair<Integer, Integer> p = relativeAddress(idx);
+        return getValue(p.getLeft(), p.getRight());
+    }
+
+    @VisibleForTesting
+    void put(int idx, K key, V value) {
+        // a put with nulls is a delete
+        // delete and shift might transport null values
+        if (key == null && value == null && isFull(idx)) {
+            markEmpty(idx);
+            this.numEmptySlots++;
+        } else {
+            markFull(idx);
+            this.numEmptySlots--;
+        }
+        putKey(idx, key);
+        putValue(idx, value);
+    }
+
+    private void putKey(int idx, K key) {
+        Pair<Integer, Integer> p = relativeAddress(idx);
+        putKey(p.getLeft(), p.getRight(), key);
+    }
+
+    private void putValue(int idx, V value) {
+        Pair<Integer, Integer> p = relativeAddress(idx);
+        putValue(p.getLeft(), p.getRight(), value);
+    }
+
+    private void putKey(int index, int offset, K key) {
+        this.keys.get(index).set(offset, key);
+    }
+
+    private void putValue(int index, int offset, V value) {
+        this.values.get(index).set(offset, value);
+    }
+}
---
+++ dist-bc-service/src/test/java/org/distbc/data/structures/cct/InternalNodeGroupTest.java
@@ -0,0 +1,140 @@
+package org.distbc.data.structures.cct;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class InternalNodeGroupTest {
+
+    private final int nodeSize = 3;
+    private final int numNodes = 4;
+
+    @Test
+    public void testSplit() {
+        InternalNodeGroup<Integer> ing = new InternalNodeGroup<>(1, nodeSize, numNodes);
+        for (int i = 0; i < numNodes * nodeSize; i++) {
+            assertTrue(ing.hasEmptySlots());
+            ing.put(i, i * 3);
+            assertTrue(ing.isFull(i));
+        }
+
+        assertFalse(ing.hasEmptySlots());
+
+        InternalNodeGroup<Integer> newIng = ing.split();
+        assertTrue(ing.hasEmptySlots());
+        assertTrue(newIng.hasEmptySlots());
+        assertEquals((nodeSize * numNodes) / 2, ing.getEmptySlots());
+        assertEquals((nodeSize * numNodes) / 2, newIng.getEmptySlots());
+
+        for (int i = 0; i < numNodes * nodeSize; i++) {
+            boolean shouldBeFull = (i < (numNodes * nodeSize) / 2);
+            assertEquals(shouldBeFull, ing.isFull(i));
+            assertEquals(shouldBeFull, newIng.isFull(i));
+        }
+    }
+
+    @Test
+    public void testSplitWithChildNodes() {
+        LeafNodeGroup<Integer, String> lng = getFullLeafNodeGroup();
+        InternalNodeGroup<Integer> ing = new InternalNodeGroup<>(1, nodeSize, numNodes);
+        ing.setChildNode(0, lng);
+
+        if(!lng.hasEmptySlots()) {
+            LeafNodeGroup<Integer, String> newLng = lng.split();
+            // the index is coming from the search
+            int nodeIdx = ing.findIndexOfEmptyNodeFrom(3);
+            ing.setChildNode(nodeIdx, newLng);
+        }
+
+        int idx = lng.findClosestEmptySlotFrom(0);
+        assertTrue(lng.isFull(0));
+        lng.shiftOneRight(0, idx);
+        assertFalse(lng.isFull(0));
+        lng.put(0, -1, "prefix_-1");
+    }
+
+    private LeafNodeGroup<Integer, String> getFullLeafNodeGroup() {
+        LeafNodeGroup<Integer, String> lng = new LeafNodeGroup<>(nodeSize, numNodes);
+        for (int i = 0; i < numNodes * nodeSize; i++) {
+            assertTrue(lng.hasEmptySlots());
+            lng.put(i, i, "prefix_" + i);
+            assertTrue(lng.isFull(i));
+        }
+        return lng;
+    }
+
+    @Test
+    public void testShiftWithinNode() {
+        InternalNodeGroup<Integer> ing = new InternalNodeGroup<>(1, nodeSize, numNodes);
+        ing.put(1, 17);
+        assertTrue(ing.isFull(1));
+
+        ing.shiftOneRight(1, 4);
+        assertFalse(ing.isFull(1));
+        assertTrue(ing.isFull(2));
+
+        assertEquals(Integer.valueOf(17), ing.getKey(2));
+        assertEquals((nodeSize * numNodes) - 1, ing.getEmptySlots());
+    }
+
+    @Test
+    public void testShiftAcrossNodes() {
+        InternalNodeGroup<Integer> ing = new InternalNodeGroup<>(1, nodeSize, numNodes);
+        ing.put(2, 17);
+        assertTrue(ing.isFull(2));
+
+        ing.shiftOneRight(1, 4);
+        assertFalse(ing.isFull(2));
+        assertTrue(ing.isFull(3));
+
+        assertEquals(Integer.valueOf(17), ing.getKey(3));
+        assertEquals((nodeSize * numNodes) - 1, ing.getEmptySlots());
+    }
+
+    @Test
+    public void testShiftingAGroup() {
+        InternalNodeGroup<Integer> ing = new InternalNodeGroup<>(1, nodeSize, numNodes);
+        ing.put(2, 17);
+        ing.put(3, 18);
+        ing.put(4, 19);
+        ing.put(5, 20);
+
+        assertTrue(ing.isFull(2));
+        assertTrue(ing.isFull(3));
+        assertTrue(ing.isFull(4));
+        assertTrue(ing.isFull(5));
+        assertFalse(ing.isFull(6));
+
+        ing.shiftOneRight(2, 6);
+
+        assertFalse(ing.isFull(2));
+        assertTrue(ing.isFull(3));
+        assertTrue(ing.isFull(4));
+        assertTrue(ing.isFull(5));
+        assertTrue(ing.isFull(6));
+    }
+
+    @Test
+    public void testFindNextEmptyNode() {
+        InternalNodeGroup<Integer> ing = new InternalNodeGroup<>(1, nodeSize, numNodes);
+        for (int i = 0; i < nodeSize; i++) {
+            assertTrue(ing.hasEmptySlots());
+            int idx = nodeSize + i;
+            ing.put(idx, i * 3);
+            assertTrue(ing.isFull(idx));
+        }
+
+        assertEquals(6, ing.findIndexOfEmptyNodeFrom(1));
+
+        for (int i = 3 * nodeSize; i < nodeSize * numNodes; i++) {
+            assertTrue(ing.hasEmptySlots());
+            int idx = (3 * nodeSize) + i;
+            ing.put(idx, i * 3);
+            assertTrue(ing.isFull(idx));
+        }
+
+        assertEquals(-1, ing.findIndexOfEmptyNodeFrom(10));
+    }
+}
