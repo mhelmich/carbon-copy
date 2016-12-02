414e3e036eef9a9da015062a42fb182c2348597a
Marco Helmich <mhelmich@lendingclub.com>
2016-12-01 23:16:38 -0800

    - second cleaner implementation -- coding up simplest puts and gets in the tree and adding test coverage for that too

--- dist-bc-service/src/main/java/org/distbc/data/structures/cct/Tree.java
+++ dist-bc-service/src/main/java/org/distbc/data/structures/cct/Tree.java
@@ -1,18 +1,128 @@
 package org.distbc.data.structures.cct;
 
-import java.util.Collections;
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class Tree<K extends Comparable<K>, V extends Comparable<V>> {
-    public void put(K key, V value) {
 
+    private final int numberOfNodesInInternalNodeGroup;
+    private final int numberOfNodesInLeafNodeGroup;
+    private final int internalNodeSize;
+    private final int leafNodeSize;
+
+    private InternalNodeGroup<K> root;
+
+    @VisibleForTesting
+    Tree(int leafNodeSize, int numberOfNodesInLeafNodeGroup) {
+        this.numberOfNodesInLeafNodeGroup = numberOfNodesInLeafNodeGroup;
+        this.numberOfNodesInInternalNodeGroup = leafNodeSize - 1;
+        this.internalNodeSize = numberOfNodesInLeafNodeGroup - 1;
+        this.leafNodeSize = leafNodeSize;
+
+        root = new InternalNodeGroup<>(1, this.internalNodeSize, this.numberOfNodesInInternalNodeGroup);
+        LeafNodeGroup<K, V> lng = new LeafNodeGroup<>(this.leafNodeSize, this.numberOfNodesInLeafNodeGroup);
+        root.setChildNodeOnNode(0, lng);
+    }
+
+    @VisibleForTesting
+    Tree(int leafNodeSize, int numberOfNodesInLeafNodeGroup, InternalNodeGroup<K> root) {
+        this.numberOfNodesInLeafNodeGroup = numberOfNodesInLeafNodeGroup;
+        this.numberOfNodesInInternalNodeGroup = leafNodeSize - 1;
+        this.internalNodeSize = numberOfNodesInLeafNodeGroup - 1;
+        this.leafNodeSize = leafNodeSize;
+
+        this.root = root;
+    }
+
+    public synchronized void put(K key, V value) {
+        List<InternalNodeGroup<K>> nodeTrace = new LinkedList<>();
+        LeafNodeGroup<K, V> lng = searchLeafNodeGroup(key, root, /* inout */ nodeTrace);
+        int insertionIdx = findInsertionIndex(key, lng);
+        lng.maybeShiftOneRight(insertionIdx);
+        lng.put(insertionIdx, key, value);
     }
 
     public Set<V> get(K key) {
-        return Collections.emptySet();
+        List<InternalNodeGroup<K>> nodeTrace = new LinkedList<>();
+        LeafNodeGroup<K, V> lng = searchLeafNodeGroup(key, root, /* inout */ nodeTrace);
+        int idx = findIndexOfFirstKey(key, lng);
+        Set<V> resultSet = new HashSet<>();
+        while (lng.getKey(idx) != null && lng.getKey(idx).equals(key)) {
+            resultSet.add(lng.getValue(idx));
+            idx++;
+        }
+        return resultSet;
     }
 
     public void delete(K key) {
+        put(key, null);
+    }
 
+    @VisibleForTesting
+    LeafNodeGroup<K, V> searchLeafNodeGroup(K key, InternalNodeGroup<K> ing, /* inout */ List<InternalNodeGroup<K>> nodeTrace) {
+        int idx;
+        InternalNodeGroup<K> ng = ing;
+
+        while (ng.getLevel() > 1) {
+            idx = findIndexOfNextNodeGroup(key, ng);
+            // I can do that because I know better
+            // level > 1 :)
+            nodeTrace.add(ng);
+            ng = (InternalNodeGroup<K>) ng.getChild(idx);
+        }
+
+        nodeTrace.add(ng);
+        // ng has now a level 1 (aka the next child pointer will be the leaf)
+        idx = findIndexOfNextNodeGroup(key, ng);
+        return (LeafNodeGroup<K, V>) ng.getChild(idx);
+    }
+
+    private int findIndexOfNextNodeGroup(K key, InternalNodeGroup<K> ing) {
+        // if ing#getKey returns null, we want to return
+        // hence null greater needs to be true
+        if (ObjectUtils.compare(key, ing.getKey(0), true) < 0) {
+            return 0;
+        }
+
+        if (ObjectUtils.compare(key, ing.getKey(this.internalNodeSize - 1)) > 0) {
+            return this.internalNodeSize;
+        }
+
+        int i = 0;
+        while (ObjectUtils.compare(key, ing.getKey(i)) > 0) {
+            i++;
+        }
+
+        return i;
+    }
+
+    private int findInsertionIndex(K key, LeafNodeGroup<K, V> lng) {
+        return findIndexOfFirstKey(key, lng);
+    }
+
+    private int findIndexOfFirstKey(K key, LeafNodeGroup<K, V> lng) {
+        // if ing#getKey returns null, we want to return
+        // hence null greater needs to be true
+        if (ObjectUtils.compare(key, lng.getKey(0), true) < 0) {
+            return 0;
+        }
+
+        // the order in which the arguments are passed in seems to flip the semantic
+        // of the boolean parameter "nullGreater"
+        if (ObjectUtils.compare(lng.getKey(this.leafNodeSize - 1), key) > 0) {
+            return this.leafNodeSize - 1;
+        }
+
+        int i = 0;
+        while (ObjectUtils.compare(key, lng.getKey(i)) > 0) {
+            i++;
+        }
+
+        return i;
     }
 }
--- dist-bc-service/src/test/java/org/distbc/data/structures/cct/TreeTest.java
+++ dist-bc-service/src/test/java/org/distbc/data/structures/cct/TreeTest.java
@@ -1,4 +1,66 @@
 package org.distbc.data.structures.cct;
 
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class TreeTest {
+
+    private int leafNodeSize = 3;
+    private int numberOfNodesInLeafNodeGroup = 3;
+    private int internalNodeSize = 2;
+    private int numberOfNodesInInternalNodeGroup = 2;
+
+    @Test
+    public void testBasic() {
+        String value = "prefix_1";
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        t.put(1, value);
+        assertTrue(t.get(1).contains(value));
+    }
+
+    @Test
+    public void testSearch() {
+        List<InternalNodeGroup<Integer>> nodeTrace = new LinkedList<>();
+        InternalNodeGroup<Integer> ing = new InternalNodeGroup<>(1,internalNodeSize, numberOfNodesInInternalNodeGroup);
+
+        LeafNodeGroup<Integer, String> lng1 = getFullLeafNodeGroup(1);
+        LeafNodeGroup<Integer, String> lng2 = getFullLeafNodeGroup(10);
+
+        ing.setChildNodeOnNode(0, lng1);
+        ing.setChildNodeOnNode(1, lng2);
+
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup, ing);
+        LeafNodeGroup<Integer, String> lng = t.searchLeafNodeGroup(9, ing, nodeTrace);
+        assertEquals(lng1, lng);
+
+        Set<String> rs = t.get(9);
+        assertEquals(1, rs.size());
+        assertTrue(rs.contains("prefix_9"));
+    }
+
+    @Test
+    public void testPut() {
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        t.put(19, "prefix_19");
+        Set<String> rs = t.get(19);
+        assertEquals(1, rs.size());
+        assertTrue(rs.contains("prefix_19"));
+    }
+
+    private LeafNodeGroup<Integer, String> getFullLeafNodeGroup(int seed) {
+        LeafNodeGroup<Integer, String> lng = new LeafNodeGroup<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        for (int i = 0; i < leafNodeSize * numberOfNodesInLeafNodeGroup; i++) {
+            assertTrue(lng.hasEmptySlots());
+            int n = (seed + i) * 3;
+            lng.put(i, n, "prefix_" + n);
+            assertTrue(lng.isFull(i));
+        }
+        return lng;
+    }
 }
