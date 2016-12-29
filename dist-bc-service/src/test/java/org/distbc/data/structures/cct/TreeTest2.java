9712aeb01d62ae9406327fd72fe757cfa8364456
Marco <marco.helmich@live.com>
2016-12-29 14:46:31 -0800

    third even cleaner implementation - add more tests

--- dist-bc-service/src/test/java/org/distbc/data/structures/cct/TreeTest2.java
+++ dist-bc-service/src/test/java/org/distbc/data/structures/cct/TreeTest2.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -58,6 +59,114 @@ public class TreeTest2 {
         m.entrySet().forEach(e -> {
                 Set<String> s = t.get(e.getKey());
                 assertEquals("at " + e.toString(), 1, s.size());
+                assertTrue(s.contains(e.getValue()));
+            }
+        );
+    }
+
+    @Test
+    public void testPutGetSameKey() {
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        t.put(19, "prefix_19");
+        t.put(19, "prefix_20");
+        t.put(19, "prefix_21");
+        Set<String> rs = t.get(19);
+        assertEquals(3, rs.size());
+        assertTrue(rs.contains("prefix_19"));
+        assertTrue(rs.contains("prefix_20"));
+        assertTrue(rs.contains("prefix_21"));
+    }
+
+    @Test
+    public void testPutGetDecreasingOrder() {
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        Map<Integer, String> m = new HashMap<>();
+        int numElements = leafNodeSize * numberOfNodesInLeafNodeGroup;
+        for (int i = numElements; i > 0; i--) {
+            String str = "prefix_" + i;
+            t.put(i, str);
+            m.put(i, str);
+        }
+
+        System.err.println(t.toString());
+
+        m.entrySet().forEach(e -> {
+                Set<String> s = t.get(e.getKey());
+                assertEquals("at " + e.toString(), 1, s.size());
+                assertTrue(s.contains(e.getValue()));
+            }
+        );
+    }
+
+    @Test
+    public void testPutGetAlternatingOrder() {
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        Map<Integer, String> m = new HashMap<>();
+        int numElements = (leafNodeSize * numberOfNodesInLeafNodeGroup) / 2;
+        for (int i = 0; i < numElements; i++) {
+            int num = i;
+            String str = "prefix_" + num;
+            t.put(num, str);
+            m.put(num, str);
+
+            num = -i;
+            str = "prefix_" + num;
+            t.put(num, str);
+            m.put(num, str);
+        }
+
+        System.err.println(t.toString());
+
+        m.entrySet().forEach(e -> {
+                Set<String> s = t.get(e.getKey());
+                assertEquals("at " + e.toString(), 1, s.size());
+                assertTrue(s.contains(e.getValue()));
+            }
+        );
+    }
+
+    @Test
+    public void testPutGetRandomKeys() {
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        Map<Integer, String> m = new HashMap<>();
+        int numElements = leafNodeSize * numberOfNodesInLeafNodeGroup;
+        Random random = new Random();
+
+        for (int i = 0; i < numElements; i++) {
+            int num = random.nextInt(1000);
+            String str = "prefix_" + num;
+            t.put(num, str);
+            m.put(num, str);
+        }
+
+        m.entrySet().forEach(e -> {
+                Set<String> s = t.get(e.getKey());
+                assertEquals(1, s.size());
+                assertTrue(s.contains(e.getValue()));
+            }
+        );
+    }
+
+    @Test
+    public void testSplittingSplits() {
+        Tree<Integer, String> t = new Tree<>(leafNodeSize, numberOfNodesInLeafNodeGroup);
+        Map<Integer, String> m = new HashMap<>();
+        // yea yea, I know
+        // the point is:
+        // I can only insert few elements as I though because the split is right leaning
+        // once I change that I can put more
+        // however, just don't change the sizes and this test will pass
+        int numElements = (int) Math.floor(10 * leafNodeSize * numberOfNodesInLeafNodeGroup);
+
+        for (int i = 0; i < numElements; i++) {
+            String str = "prefix_" + i;
+            t.put(i, str);
+            m.put(i, str);
+        }
+
+        m.entrySet().forEach(e -> {
+                Set<String> s = t.get(e.getKey());
+                assertEquals(1, s.size());
                 assertTrue(s.contains(e.getValue()));
             }
         );
