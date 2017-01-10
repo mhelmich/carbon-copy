53baed178d0c7460c189f6b2b9695d6ae5f6989f
Marco <marco.helmich@live.com>
2017-01-09 20:20:30 -0800

    - overriding equals and hashcode

--- dist-bc-service/src/main/java/org/distbc/data/structures/sibplustree/NodeIdxAndIdx.java
+++ dist-bc-service/src/main/java/org/distbc/data/structures/sibplustree/NodeIdxAndIdx.java
@@ -15,6 +15,24 @@ class NodeIdxAndIdx {
         return nodeIdx + "_" + idx;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !NodeIdxAndIdx.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+
+        NodeIdxAndIdx other = (NodeIdxAndIdx) obj;
+        return this.nodeIdx == other.nodeIdx && this.idx == other.idx;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = hash * 53 + nodeIdx;
+        hash = hash * 53 + idx;
+        return hash;
+    }
+
     static NodeIdxAndIdx of(int nodeIdx, int idx) {
         return new NodeIdxAndIdx(nodeIdx, idx);
     }
