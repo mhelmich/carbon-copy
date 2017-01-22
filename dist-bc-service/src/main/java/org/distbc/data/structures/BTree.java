f116fc87ff1543925ee15431ce3edce547876c98
Marco Helmich <mhelmich@lendingclub.com>
2017-01-21 18:41:49 -0800

    - introducing Roberts BTree

--- dist-bc-service/src/main/java/org/distbc/data/structures/BTree.java
+++ dist-bc-service/src/main/java/org/distbc/data/structures/BTree.java
@@ -1,19 +1,13 @@
 package org.distbc.data.structures;
 
-/******************************************************************************
- *  Compilation:  javac BTree.java
- *  Execution:    java BTree
- *  Dependencies: StdOut.java
- *
- *  B-tree.
- *
- *  Limitations
- *  -----------
- *   -  Assumes M is even and M >= 4
- *   -  should b be an array of children or list (it would help with
- *      casting to make it a list)
- *
- ******************************************************************************/
+/*
+ B-tree.
+
+ Limitations
+ -----------
+ -  Assumes M is even and M >= 4
+
+ */
 
 import java.util.ArrayList;
 import java.util.Vector;
@@ -251,26 +245,27 @@ public class BTree<Key extends Comparable<Key>, Value>  {
     }
 }
 
-/******************************************************************************
- *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
+/*
+ * ****************************************************************************
+ Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
+
+ This file is part of algs4.jar, which accompanies the textbook
+
+ Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ http://algs4.cs.princeton.edu
+
+
+ algs4.jar is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ algs4.jar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ */
