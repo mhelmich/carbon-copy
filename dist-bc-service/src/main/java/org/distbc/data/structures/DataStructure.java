9ae21b6bb2abcc597fdcf90f704b85308f49c8cb
Marco <marco.helmich@live.com>
2017-01-23 16:05:32 -0800

    - built the kryo stream abstraction   - looks pretty neat

--- dist-bc-service/src/main/java/org/distbc/data/structures/DataStructure.java
+++ dist-bc-service/src/main/java/org/distbc/data/structures/DataStructure.java
@@ -5,6 +5,8 @@ import co.paralleluniverse.common.io.ByteBufferOutputStream;
 import co.paralleluniverse.common.io.Persistable;
 import co.paralleluniverse.galaxy.Grid;
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import com.esotericsoftware.kryo.pool.KryoFactory;
 import com.esotericsoftware.kryo.pool.KryoPool;
 import com.google.inject.Inject;
@@ -21,19 +23,36 @@ abstract class DataStructure implements Persistable {
 
     private static KryoFactory kryoFactory = () -> {
         Kryo kryo = new Kryo();
+        kryo.setRegistrationRequired(true);
         // configure kryo instance, customize settings
-        kryo.register(SkipList.class, 0);
-        kryo.register(BTree.class, 1);
+//        kryo.register(Integer.class, 0);
+//        kryo.register(String.class, 1);
+//        kryo.register(SkipList.class);
+//        kryo.register(BTree.class);
+
+        // the first few integers are taken (0 .. 9)
+        // these are obviously kryo internals and subject to change at any point in time
+        // [0=[0, int], 9=[9, void], 8=[8, double], 7=[7, long], 6=[6, short], 5=[5, char],
+        //  4=[4, byte], 3=[3, boolean], 2=[2, float], 1=[1, String]]
+
         return kryo;
     };
 
-    static KryoPool kryoPool = new KryoPool.Builder(kryoFactory).build();
+    private static KryoPool kryoPool = new KryoPool.Builder(kryoFactory).build();
 
     @Inject
     Grid grid;
 
+    private long id;
+
+    DataStructure() {}
+
+    DataStructure(long id) {
+        this.id = id;
+    }
+
     public void write(ByteBuffer bb) {
-        try (OutputStream out = new ByteBufferOutputStream(bb)) {
+        try (KryoOutputStream out = new KryoOutputStream(new ByteBufferOutputStream(bb))) {
             serialize(out);
         } catch (IOException xcp) {
             throw new RuntimeException(xcp);
@@ -41,13 +60,77 @@ abstract class DataStructure implements Persistable {
     }
 
     public void read(ByteBuffer bb) {
-        try (InputStream in = new ByteBufferInputStream(bb)) {
+        try (KryoInputStream in = new KryoInputStream(new ByteBufferInputStream(bb))) {
             deserialize(in);
         } catch (IOException xcp) {
             throw new RuntimeException(xcp);
         }
     }
 
-    abstract void serialize(OutputStream out);
-    abstract void deserialize(InputStream out);
+    abstract void serialize(KryoOutputStream out);
+    abstract void deserialize(KryoInputStream out);
+
+    static class KryoOutputStream extends OutputStream {
+        private Kryo kryo;
+        private Output out;
+
+        private KryoOutputStream(OutputStream o) {
+            out = new Output(o);
+            kryo = kryoPool.borrow();
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            out.write(b);
+        }
+
+        void writeObject(Object o) {
+            kryo.writeClassAndObject(out, o);
+        }
+
+        @Override
+        public void close() throws IOException {
+            try {
+                out.close();
+            } finally {
+                try {
+                    kryoPool.release(kryo);
+                } finally {
+                    super.close();
+                }
+            }
+        }
+    }
+
+    static class KryoInputStream extends InputStream {
+        private final Kryo kryo;
+        private final Input in;
+
+        private KryoInputStream(InputStream i) {
+            in = new Input(i);
+            kryo = kryoPool.borrow();
+        }
+
+        @Override
+        public int read() throws IOException {
+            return in.read();
+        }
+
+        Object readObject() {
+            return kryo.readClassAndObject(in);
+        }
+
+        @Override
+        public void close() throws IOException {
+            try {
+                in.close();
+            } finally {
+                try {
+                    kryoPool.release(kryo);
+                } finally {
+                    super.close();
+                }
+            }
+        }
+    }
 }
--- dist-bc-service/src/test/java/org/distbc/data/structures/DataBlockTest.java
+++ dist-bc-service/src/test/java/org/distbc/data/structures/DataBlockTest.java
@@ -3,6 +3,7 @@ package org.distbc.data.structures;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -38,7 +39,7 @@ public class DataBlockTest {
         assertEquals(Integer.valueOf(5), db.get(5));
         assertEquals(Integer.valueOf(6), db.get(6));
 
-        int cap = 16;
+        int cap = 10;
         ByteBuffer bb = ByteBuffer.allocate(cap);
         db.write(bb);
         System.err.println("byte size: " + bb.position());
@@ -50,5 +51,26 @@ public class DataBlockTest {
 
         assertEquals(Integer.valueOf(5), db2.get(5));
         assertEquals(Integer.valueOf(6), db2.get(6));
+
+        DataBlock<String, String> db3 = new DataBlock<>();
+        String val1 = UUID.randomUUID().toString();
+        String val2 = UUID.randomUUID().toString();
+        String val3 = UUID.randomUUID().toString();
+        db3.put("1", val1);
+        db3.put("2", val2);
+        db3.put("3", val3);
+
+        cap = 4096;
+        ByteBuffer bb2 = ByteBuffer.allocate(cap);
+        db3.write(bb2);
+        System.err.println("byte size: " + bb2.position());
+        assertTrue(cap > bb2.remaining());
+        bb2.rewind();
+
+        DataBlock<String, String> db4 = new DataBlock<>();
+        db4.read(bb2);
+        assertEquals(val2, db4.get("2"));
+        assertEquals(val3, db4.get("3"));
+        assertEquals(val1, db4.get("1"));
     }
 }
