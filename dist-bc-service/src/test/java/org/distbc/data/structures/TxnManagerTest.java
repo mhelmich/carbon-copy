42334dc045a9089f7245695de2b6feab255a05d5
Marco <marco.helmich@live.com>
2017-01-31 17:07:20 -0800

    - boosting test coverage

--- dist-bc-service/src/test/java/org/distbc/data/structures/TxnManagerTest.java
+++ dist-bc-service/src/test/java/org/distbc/data/structures/TxnManagerTest.java
@@ -10,6 +10,10 @@ import org.junit.runner.RunWith;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(GuiceJUnit4Runner.class)
 @GuiceModules({ DataStructureModule.class, TxnManagerModule.class })
@@ -22,10 +26,10 @@ public class TxnManagerTest {
     private TxnManager txnManager;
 
     @Test
-    @Ignore
     public void testBasicTransaction() throws Exception {
         int count = 100;
         Map<String, String> map = new HashMap<>();
+        AtomicLong id = new AtomicLong(-1L);
 
         txnManager.doTransactionally(txn -> {
             DataBlock<String, String> db = dsFactory.newDataBlock(txn);
@@ -35,25 +39,37 @@ public class TxnManagerTest {
 
                 map.put(key, value);
                 db.put(key, value, txn);
+                id.set(db.getId());
             }
         });
+
+        DataBlock<String, String> db = dsFactory.loadDataBlock(id.get());
+        map.entrySet().forEach(e -> assertEquals(e.getValue(), db.get(e.getKey())));
     }
 
+    /**
+     * Rolling back an empty object fails with an NPE.
+     * I still need to find a way around that...
+     */
     @Test
     @Ignore
     public void testChangingExistingHash() throws Exception {
-        int count = 100;
-        Map<String, String> map = new HashMap<>();
+        AtomicLong id = new AtomicLong(-1L);
 
-        ChainingHash<String, String> hash = dsFactory.newChainingHash();
         txnManager.doTransactionally(txn -> {
-            for (int i = 0; i < count; i++) {
-                String key = UUID.randomUUID().toString();
+            DataBlock<Integer, String> db = dsFactory.newDataBlock(txn);
+            for (int i = 0; i < 10; i++) {
                 String value = UUID.randomUUID().toString();
 
-                map.put(key, value);
-                hash.put(value, key, txn);
+                db.put(i, value, txn);
+                id.set(db.getId());
             }
+            throw new RuntimeException("BOOOOM -- this has been planted for your test");
         });
+
+        DataBlock<Integer, String> db = dsFactory.loadDataBlock(id.get());
+        assertNull(db.get(7));
+        assertNull(db.get(3));
+        assertNull(db.get(5));
     }
 }
