1a656d84cc284881d38db1959470ac6cd18110f5
Marco <marco.helmich@live.com>
2017-02-02 17:13:24 -0800

    - prepared interfaces and APIs - tests still pass but no galaxy functionality yet

--- dist-bc-service/src/main/java/org/distbc/data/structures/BTreeNode.java
+++ dist-bc-service/src/main/java/org/distbc/data/structures/BTreeNode.java
@@ -1,21 +1,25 @@
 package org.distbc.data.structures;
 
+import co.paralleluniverse.galaxy.Store;
+
 import java.util.ArrayList;
 import java.util.Vector;
 
 /**
  * Helper and container for a bunch of items that belong into a tree
  */
-class BTreeNode<Key extends Comparable<Key>, Value> {
+class BTreeNode<Key extends Comparable<Key>, Value> extends DataStructure {
     private int numChildren;
     // pointers to children
     private ArrayList<BTreeEntry<Key, Value>> children;
 
-    BTreeNode(int numChildren) {
+    BTreeNode(Store store, int numChildren, Txn txn) {
+        super(store);
         Vector<BTreeEntry<Key, Value>> v = new Vector<>(BTree.MAX_NODE_SIZE);
         v.setSize(BTree.MAX_NODE_SIZE);
         children = new ArrayList<>(v);
         this.numChildren = numChildren;
+        asyncUpsert(this, txn);
     }
 
     int getNumChildren() {
@@ -28,5 +32,19 @@ class BTreeNode<Key extends Comparable<Key>, Value> {
 
     ArrayList<BTreeEntry<Key, Value>> getChildren() {
         return children;
+    }
+
+    /////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////
+    // galaxy-specific serialization overrides
+
+    @Override
+    void serialize(SerializerOutputStream out) {
+
+    }
+
+    @Override
+    void deserialize(SerializerInputStream in) {
+
     }
 }
--- dist-bc-service/src/test/java/org/distbc/data/structures/BTreeTest.java
+++ dist-bc-service/src/test/java/org/distbc/data/structures/BTreeTest.java
@@ -17,24 +17,25 @@ public class BTreeTest {
     @Test
     public void testBasic() {
         BTree<String, String> t = newTree();
+        Txn txn = Mockito.mock(Txn.class);
 
-        t.put("www.cs.princeton.edu", "128.112.136.12");
-        t.put("www.cs.princeton.edu", "128.112.136.11");
-        t.put("www.princeton.edu",    "128.112.128.15");
-        t.put("www.yale.edu",         "130.132.143.21");
-        t.put("www.simpsons.com",     "209.052.165.60");
-        t.put("www.apple.com",        "17.112.152.32");
-        t.put("www.amazon.com",       "207.171.182.16");
-        t.put("www.ebay.com",         "66.135.192.87");
-        t.put("www.cnn.com",          "64.236.16.20");
-        t.put("www.google.com",       "216.239.41.99");
-        t.put("www.nytimes.com",      "199.239.136.200");
-        t.put("www.microsoft.com",    "207.126.99.140");
-        t.put("www.dell.com",         "143.166.224.230");
-        t.put("www.slashdot.org",     "66.35.250.151");
-        t.put("www.espn.com",         "199.181.135.201");
-        t.put("www.weather.com",      "63.111.66.11");
-        t.put("www.yahoo.com",        "216.109.118.65");
+        t.put("www.cs.princeton.edu", "128.112.136.12", txn);
+        t.put("www.cs.princeton.edu", "128.112.136.11", txn);
+        t.put("www.princeton.edu",    "128.112.128.15", txn);
+        t.put("www.yale.edu",         "130.132.143.21", txn);
+        t.put("www.simpsons.com",     "209.052.165.60", txn);
+        t.put("www.apple.com",        "17.112.152.32", txn);
+        t.put("www.amazon.com",       "207.171.182.16", txn);
+        t.put("www.ebay.com",         "66.135.192.87", txn);
+        t.put("www.cnn.com",          "64.236.16.20", txn);
+        t.put("www.google.com",       "216.239.41.99", txn);
+        t.put("www.nytimes.com",      "199.239.136.200", txn);
+        t.put("www.microsoft.com",    "207.126.99.140", txn);
+        t.put("www.dell.com",         "143.166.224.230", txn);
+        t.put("www.slashdot.org",     "66.35.250.151", txn);
+        t.put("www.espn.com",         "199.181.135.201", txn);
+        t.put("www.weather.com",      "63.111.66.11", txn);
+        t.put("www.yahoo.com",        "216.109.118.65", txn);
 
         assertEquals("63.111.66.11", t.get("www.weather.com"));
         assertEquals("128.112.128.15", t.get("www.princeton.edu"));
@@ -46,11 +47,12 @@ public class BTreeTest {
         Map<Integer, String> m = new HashMap<>();
         int count = 10000;
         Random r = new Random();
+        Txn txn = Mockito.mock(Txn.class);
 
         for (int i = 0; i < count; i++) {
             int key = r.nextInt();
             String value = UUID.randomUUID().toString();
-            t.put(key, value);
+            t.put(key, value, txn);
             if (r.nextBoolean() && !m.containsKey(key)) m.put(key, value);
         }
 
@@ -62,8 +64,9 @@ public class BTreeTest {
     @Test
     public void testDups() {
         BTree<Integer, String> t = newTree();
-        t.put(5, "narf_5");
-        t.put(5, "narf_6");
+        Txn txn = Mockito.mock(Txn.class);
+        t.put(5, "narf_5", txn);
+        t.put(5, "narf_6", txn);
 
         assertEquals("narf_5", t.get(5));
     }
@@ -71,11 +74,12 @@ public class BTreeTest {
     @Test
     public void testDelete() {
         BTree<Integer, String> t = newTree();
-        t.put(5, "narf_5");
-        t.put(6, "narf_6");
-        t.put(7, "narf_7");
+        Txn txn = Mockito.mock(Txn.class);
+        t.put(5, "narf_5", txn);
+        t.put(6, "narf_6", txn);
+        t.put(7, "narf_7", txn);
 
-        t.put(6, null);
+        t.put(6, null, txn);
 
         assertEquals("narf_5", t.get(5));
         assertNull(t.get(6));
@@ -86,6 +90,6 @@ public class BTreeTest {
         Txn txn = Mockito.mock(Txn.class);
         when(txn.getStoreTransaction()).thenReturn(null);
         Store s = Mockito.mock(Store.class);
-        return new BTree<>(s, txn);
+        return new BTree<>(s, new DataStructureFactoryImpl(s), txn);
     }
 }
