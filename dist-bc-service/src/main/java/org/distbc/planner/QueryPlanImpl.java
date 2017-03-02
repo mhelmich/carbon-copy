fd8b1f97c949be36accc85d80a361599e5530dd7
Marco Helmich <mhelmich@lendingclub.com>
2017-03-02 15:08:43 -0800

    - putting in first attempt at a QueryPlanner interface

---
+++ dist-bc-service/src/main/java/org/distbc/planner/QueryPlanImpl.java
@@ -0,0 +1,11 @@
+package org.distbc.planner;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class QueryPlanImpl implements QueryPlan {
+    @Override
+    public Set<Object> execute() {
+        return Collections.emptySet();
+    }
+}
