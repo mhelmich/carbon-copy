eef3680991459246bf4995da10641efb12f0e75c
Marco <marco.helmich@live.com>
2017-05-02 23:29:59 -0700

    - renaming classes

--- dist-bc-service/src/main/java/org/distbc/planner/OpSelection.java
+++ dist-bc-service/src/main/java/org/distbc/planner/OpSelection.java
@@ -18,71 +18,80 @@
 
 package org.distbc.planner;
 
+import net.sourceforge.jeval.EvaluationException;
+import net.sourceforge.jeval.Evaluator;
 import org.distbc.data.structures.GUID;
 import org.distbc.data.structures.Table;
 import org.distbc.data.structures.Tuple;
-import org.distbc.parser.ParsingResult;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-class OpSelection implements Supplier<Set<GUID>> {
-    private final List<ParsingResult.BinaryOperation> bos;
-    private final Table tableToUse;
+public class OpSelection implements Supplier<Set<GUID>> {
 
-    OpSelection(List<ParsingResult.BinaryOperation> bos, Table tableToUse) {
-        this.bos = bos;
+    private final Set<String> columnNames;
+    private final Table tableToUse;
+    private final String expression;
+
+    OpSelection(Set<String> columnNames, Table tableToUse, String expression) {
+        this.columnNames = columnNames;
         this.tableToUse = tableToUse;
+        this.expression = sanitizeExpression(expression);
     }
 
-    @SuppressWarnings("unchecked")
+    private String sanitizeExpression(String expression) {
+        return expression
+                .replaceAll(" = ", " == ")
+                .replaceAll(" AND ", " && ")
+                .replaceAll(" OR ", " || ");
+    }
+
     @Override
     public Set<GUID> get() {
         List<Tuple> tableMetadata = tableToUse.getColumnMetadata();
         // TODO -- we might want to filter this to only columns we need
         Map<String, Integer> columnNameToIndex = tableMetadata.stream()
                 .collect(Collectors.toMap(tuple -> (String)tuple.get(0), tuple -> (Integer)tuple.get(1)));
+        Map<String, Boolean> columnNameToNumeric = tableMetadata.stream()
+                .collect(Collectors.toMap(tuple -> (String)tuple.get(0), tuple -> {
+                    try {
+                        return Number.class.isAssignableFrom(Class.forName(tuple.get(2).toString()));
+                    } catch (ClassNotFoundException e) {
+                        return false;
+                    }
+                }));
+
+        Evaluator eval = new Evaluator();
+
+        String tmp = expression;
+        for (String columnName : columnNames) {
+            Boolean isNumber = columnNameToNumeric.get(columnName);
+            if (isNumber != null && isNumber) {
+                tmp = tmp.replaceAll(columnName, "#{" + columnName + "}");
+            } else {
+                // this contains single quotes!!! to make non-numeric values a string
+                tmp = tmp.replaceAll(columnName, "'#{" + columnName + "}'");
+            }
+        }
+        // this is used into the closure and needs to be final
+        String newExpression = tmp;
+
 
         Predicate<Tuple> p = tuple -> {
-            AtomicBoolean b = new AtomicBoolean(false);
-            bos.forEach(binaryOperation -> {
-                Integer idx = columnNameToIndex.get(binaryOperation.operand1.toUpperCase());
-                if (idx != null) {
-                    int cmp = tuple.get(idx).compareTo(binaryOperation.operand2);
-                    switch(binaryOperation.operation) {
-                        case "=":
-                            b.getAndSet(cmp == 0);
-                            break;
-                        case ">":
-                            b.getAndSet(cmp > 0);
-                            break;
-                        case ">=":
-                            b.getAndSet(cmp >= 0);
-                            break;
-                        case "<":
-                            b.getAndSet(cmp < 0);
-                            break;
-                        case "<=":
-                            b.getAndSet(cmp <= 0);
-                            break;
-                        case "<>":
-                            b.getAndSet(cmp != 0);
-                            break;
-                        default:
-                            throw new IllegalArgumentException("Couldn't parse comparator " + binaryOperation.operation);
-                    }
-                } else {
-                    throw new IllegalArgumentException("Couldn't find column " + binaryOperation.operand1);
-                }
-            });
-            // if this return true, the record is being kept in the result set
-            // if this returns false, the record will be deleted from the result set
-            return b.get();
+            for (String columnName : columnNames) {
+                // TODO -- yeah sure, we could micro-optimize this by not making the lookup all the time
+                eval.putVariable(columnName, tuple.get(columnNameToIndex.get(columnName)).toString());
+            }
+
+            try {
+                return eval.getBooleanResult(newExpression);
+            } catch (EvaluationException xcp) {
+                throw new RuntimeException(xcp);
+            }
         };
 
         // do the actual selection
