4eeb4a8dba7774b280028f95f31e98acb81dff6b
Marco <marco.helmich@live.com>
2017-07-01 21:20:48 -0700

    - properly implementing a remote and an embedded jdbc driver - the jdbc server is now being started as part of the module

--- carbon-copy-service/src/main/java/org/carbon/copy/calcite/CalciteModule.java
+++ carbon-copy-service/src/main/java/org/carbon/copy/calcite/CalciteModule.java
@@ -1,6 +1,12 @@
 package org.carbon.copy.calcite;
 
 import com.google.inject.AbstractModule;
+import org.apache.calcite.avatica.jdbc.JdbcMeta;
+import org.apache.calcite.avatica.remote.LocalService;
+import org.apache.calcite.avatica.server.AvaticaJsonHandler;
+import org.apache.calcite.avatica.server.HttpServer;
+
+import java.sql.SQLException;
 
 public class CalciteModule extends AbstractModule {
     @Override
@@ -8,5 +14,21 @@ public class CalciteModule extends AbstractModule {
         // yupp, not the fine British way
         // read the comment in Injector for more context
         requestStaticInjection(Injector.class);
+
+        try {
+            HttpServer server = new HttpServer.Builder()
+                    .withHandler(new AvaticaJsonHandler(new LocalService(new JdbcMeta(EmbeddedCarbonCopyDriver.CONNECTION_PREFIX))))
+                    .withPort(8765)
+                    .build();
+            server.start();
+        } catch (SQLException xcp) {
+            throw new RuntimeException(xcp);
+        }
+
+        try {
+            Class.forName(EmbeddedCarbonCopyDriver.class.getName());
+        } catch (ClassNotFoundException xcp) {
+            throw new RuntimeException(xcp);
+        }
     }
 }
