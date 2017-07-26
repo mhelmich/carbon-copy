aba9288289c3c2071fc171ef7a824ef8bac66b6e
Marco <marco.helmich@live.com>
2017-07-26 10:06:22 -0700

    - adding comment with notes on how to proceed for intra-node transactions

--- carbon-copy-service/src/main/java/org/carbon/copy/data/structures/Txn.java
+++ carbon-copy-service/src/main/java/org/carbon/copy/data/structures/Txn.java
@@ -54,6 +54,20 @@ public class Txn {
         this.stxn = stxn;
     }
 
+    // TODO -- implement commit
+    // All changes so far have been done in memory only with the notable exception of newly created data structures
+    // This method flushes them all into the galaxy cluster.
+    // As opposed to the rest of the application this class is well aware of what cache lines are and galaxy primitives -- this class only things in terms of ids.
+    // 1. get the exclusive, node-internal transaction lock
+    // 2. get a lock on all cache lines necessary
+    //    - call asyncGetX on all of the cache lines that have changed or will be deleted
+    // 3. make all changes to the cache lines that have changed
+    // 4. call delete on all cache lines that have been deleted
+    // 5. wait for all async calls to finish
+    //    - if successful, just proceed
+    //    - if unsuccessful, roll back all other changes
+    //      - this might include deleting all newly created cache lines
+    // 6. release the exclusive, node-internal transaction lock
     public void commit() throws IOException {
         try {
             // this is a very naive implementation for now
@@ -92,6 +106,7 @@ public class Txn {
         }
     }
 
+    // TODO -- collapse this method into rollback
     public void abort() throws IOException {
         try {
             store.abort(stxn);
@@ -102,6 +117,9 @@ public class Txn {
         }
     }
 
+    // TODO -- implement rollback
+    //         - undo (delete) newly created blocks
+    //         - just toss away all other changes
     public void rollback() {
         store.rollback(stxn);
     }
